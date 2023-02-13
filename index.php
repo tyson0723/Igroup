3588ac16d796ce7e63f6b23301aff29e6e12bc70
Ryotaro Suzuki <113155939+nakamachiJP@users.noreply.github.com>
2023-02-13 12:10:57 +0900

    できた

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+<body>
+    <div style="text-align: right" class="container">
+    <a class="btn btn-border-1" href="#">ログイン</a>
+    </div>
+        <p style="text-align: center">ジョビッターへようこそ！！</p>
+        <p style="text-align: center">ここでは以下のような自由な記事を閲覧、投稿できます。</p>
+        <p style="text-align: center">ログインするとさらに多くの記事が閲覧でき、自分で記事の投稿もできます。</p><br>
+    <table id-"ta" border="1" align="center">
+        <tr>
+            <td>著者</td><td rowspan="2">タイトル</td>
+        </tr>
+        <tr>
+            <td>投稿日時</td>
+        </tr>
+        <tr>
+            <td>著者</td><td rowspan="2">タイトル</td>
+        </tr>
+        <tr>
+            <td>投稿日時</td>
+        </tr>
+        <tr>
+            <td>著者</td><td rowspan="2">タイトル</td>
+        </tr>
+        <tr>
+            <td>投稿日時</td>
+        </tr>
+        <tr>
+            <td>著者</td><td rowspan="2">タイトル</td>
+        </tr>
+        <tr>
+            <td>投稿日時</td>
+        </tr>
+        <tr>
+            <td>著者</td><td rowspan="2">タイトル</td>
+        </tr>
+        <tr>
+            <td>投稿日時</td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="./login.css">
+    </head>
+<body>
+<div class="login">
+  <div class="login-triangle"></div>
+  
+  <h2 class="login-header">ログイン</h2>
+
+  <form class="login-container">
+    <p><input type="email" placeholder="ユーザー名"></p>
+    <p><input type="password" placeholder="パスワード"></p>
+    <p><input type="submit" value="戻る"></p>
+    <p><input type="submit" value="ログイン"></p>
+    <div style="text-align: center">
+    <a href="#">新規登録はこちら</a>
+    </div>
+  </form>
+</div>
+</body>
